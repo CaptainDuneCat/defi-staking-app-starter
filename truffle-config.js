90d9666c2dfbf7b91f3891283b8fe80331d004d1
picassothecat0x <23092261+PicassoTheCat@users.noreply.github.com>
2022-05-19 02:11:26 -0700

    init commit

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+//  require('@babel/preset-env');
+// require('babel-polyfill');
+
+module.exports = {
+    networks:{
+        development:{
+            host: '127.0.0.1',
+            port: '7545',
+            network_id: '*' // connect to any network.
+        },
+    },
+    contracts_directory: './defi-staking-app-starter/src/contracts/',
+    contracts_build_directory: './defi-staking-app-starter/src/truffle_abis',
+    compilers: {
+        solc: {
+            version: '^0.5.0',
+            optimizer: {
+                enabled: true,
+                runs: 200 
+            },
+}
+    }
+}
